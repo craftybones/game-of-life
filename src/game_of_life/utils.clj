0aee2c8204fbb565c929d952fe538972e02714f0
Srijayanth Sridhar <srijayanth@gmail.com>
2016-12-31 23:01:52 +0530

    Added tests for not-nil? and until-end-of

--- src/game_of_life/utils.clj
+++ src/game_of_life/utils.clj
@@ -11,4 +11,7 @@
 
 (def not-nil? (complement nil?))
 
-(def until-end-of (comp range count))
+(def until-end-of
+  "Useful function to have when iterating over the length of a collection.
+  Supply any sequence/collection/countable as only argument."
+  (comp range count))
--- test/game_of_life/utils_test.clj
+++ test/game_of_life/utils_test.clj
@@ -5,7 +5,21 @@
 (deftest summing-vectors
   (testing "Should sum up vectors in parallel"
     (is (= [1 1] (sum-of-vectors [0 0] [1 1])))
-    (is (= [2 3 4] (sum-of-vectors [0 1 2] [2 2 2])))))
+    (is (= [2 3 4] (sum-of-vectors [0 1 2] [2 2 2])))
+    (is (= [9 12] (sum-of-vectors [1 2] [3 4] [5 6])))
+    (is (= [] (sum-of-vectors [])))))
 
-;; (deftest)
+(deftest aint-no-nil
+  (testing "Anything that isn't nil should return true and vice versa"
+    (is (not-nil? 0))
+    (is (not-nil? "a"))
+    (is (not-nil? []))
+    (is (= false (not-nil? nil)))
+    (is (= false (not-nil? (first '()))))))
+
+(deftest range-across-a-collection
+  (testing "Provides a range that ranges from 0 to the length of a collection"
+    (is (= '(0 1 2) (until-end-of [1 2 3])))
+    (is (= '(0) (until-end-of [0])))
+    (is (= '() (until-end-of [])))))
 
